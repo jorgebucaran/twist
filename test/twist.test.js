c65d34ea872351f4ff215f7f3bb0bacfdf26fbf0
Jorge Bucaran <mail@jorgebucaran.com>
2020-09-26 07:07:21 +0900

    Skip until we can bring back raw/simple easy-to-pipe-in output

--- test/twist.test.js
+++ test/twist.test.js
@@ -1,30 +1,32 @@
 import { dirname } from "path"
 import { fileURLToPath } from "url"
 import { spawnSync } from "child_process"
-import { t, deepEqual } from "../index.js"
+import { t, skip, deepEqual } from "../index.js"
 
 export default [
-  t("bin/twist", [
-    t("JSON.stringify state to stdout inside a tty", [
-      ...spawnSync("../bin/twist.js", ["fixed/loser.js"], {
-        cwd: dirname(fileURLToPath(import.meta.url)),
-        encoding: "utf8",
-      })
-        .stdout.split("\n")
-        .filter((any) => any)
-        .map(JSON.parse)
-        .map(({ suites: [{ ok, done }], failed }, i) =>
-          deepEqual(
-            [ok, done, failed],
-            i === 0
-              ? [true, false, 0] // Msg.Init
-              : i === 1
-              ? [false, false, 1] // Msg.Run
-              : i === 2
-              ? [false, true, 1] // Msg.Done
-              : []
-          )
-        ),
-    ]),
-  ]),
+  skip(
+    t("bin/twist", [
+      t("JSON.stringify state to stdout inside a tty", [
+        ...spawnSync("../bin/twist.js", ["fixed/loser.js"], {
+          cwd: dirname(fileURLToPath(import.meta.url)),
+          encoding: "utf8",
+        })
+          .stdout.split("\n")
+          .filter((any) => any)
+          .map(JSON.parse)
+          .map(({ suites: [{ ok, done }], failed }, i) =>
+            deepEqual(
+              [ok, done, failed],
+              i === 0
+                ? [true, false, 0] // Msg.Init
+                : i === 1
+                ? [false, false, 1] // Msg.Run
+                : i === 2
+                ? [false, true, 1] // Msg.Done
+                : []
+            )
+          ),
+      ]),
+    ])
+  ),
 ]
